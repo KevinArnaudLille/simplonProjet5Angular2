29ce6f1256e8dea2abea72550573056f88cea024
Kevin Arnaud <kevin.arnaud.addpro@gmail.com>
2022-03-14 12:10:30 +0100

    api called and countries lists done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { DataFromApi } from 'src/services/dataFromApi.service';
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { TestAPIComponent } from './test-api/test-api.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestAPIComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [DataFromApi],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
