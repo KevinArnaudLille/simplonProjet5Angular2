6540b1f8efe571b7476d01b4a99ad20651558997
realbenben <b.patou59@gmail.com>
2022-03-14 12:21:43 +0100

    fix b utton +create flag container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ReturnBtnComponent } from './return-btn/return-btn.component';
 import { LogoComponent } from './logo/logo.component';
+import { FlagContainerComponent } from './flag-container/flag-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ReturnBtnComponent,
-    LogoComponent
+    LogoComponent,
+    FlagContainerComponent
   ],
   imports: [
     BrowserModule
