3d8262c2291ef130cba4349c89d8d877ab863386
realbenben <b.patou59@gmail.com>
2022-03-14 16:34:16 +0100

    add nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { DataFromApi } from 'src/services/dataFromApi.service';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { TestAPIComponent } from './test-api/test-api.component';
 import { ReturnBtnComponent } from './return-btn/return-btn.component';
 import { LogoComponent } from './logo/logo.component';
 import { FlagContainerComponent } from './flag-container/flag-container.component';
 import { HomeComponent } from './home/home.component';
-import { TemlateFlagComponent } from './temlate-flag/temlate-flag.component';
 import { TemplateContinentComponent } from './template-continent/template-continent.component';
 import { TemplateCountryComponent } from './template-country/template-country.component';
 import { TemplateCapitalComponent } from './template-capital/template-capital.component';
 import { BasicTileComponent } from './basic-tile/basic-tile.component';
-
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -23,18 +22,20 @@ import { BasicTileComponent } from './basic-tile/basic-tile.component';
     LogoComponent,
     FlagContainerComponent,
     HomeComponent,
-    TemlateFlagComponent,
     TemplateContinentComponent,
     TemplateCountryComponent,
-    TemplateCapitalComponent
-    BasicTileComponent
-
+    TemplateCapitalComponent,
+    BasicTileComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    RouterModule.forRoot([
+      { path: 'template-continent', component: TemplateContinentComponent },
+      { path: 'template-country', component: TemplateCountryComponent },
+      { path: 'template-capital', component: TemplateCapitalComponent },
+    ]),
   ],
   providers: [DataFromApi],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
