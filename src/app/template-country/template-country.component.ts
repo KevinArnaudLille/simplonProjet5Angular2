2e5f0560bc14ee50bd3cb071594ab211efc8b2f6
realbenben <b.patou59@gmail.com>
2022-03-15 11:19:52 +0100

    commit before pull

--- src/app/template-country/template-country.component.ts
+++ src/app/template-country/template-country.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-template-country',
   templateUrl: './template-country.component.html',
-  styleUrls: ['./template-country.component.scss']
+  styleUrls: ['./template-country.component.scss'],
 })
 export class TemplateCountryComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
