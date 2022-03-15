2b28aac331ceac9757ebc7afdb94452127aff89b
Kevin Arnaud <kevin.arnaud.addpro@gmail.com>
2022-03-15 15:39:20 +0100

    some animations add

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  toggleCapital: boolean = false;
+  toggleContinent: boolean = false;
+  toggleFlag: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  showCapital() {
+    if (!this.toggleCapital) {
+      this.toggleCapital = !this.toggleCapital;
+      setTimeout(() => this.toggleCapital = !this.toggleCapital, 1500)
+    }
+  }
+
+  showContinent() {
+    if (!this.toggleContinent) {
+      this.toggleContinent = !this.toggleContinent;
+      setTimeout(() => this.toggleContinent = !this.toggleContinent, 1500)
+    }
+  }
+  
+  showFlag() {
+    if (!this.toggleFlag) {
+      this.toggleFlag = !this.toggleFlag;
+      setTimeout(() => this.toggleFlag = !this.toggleFlag, 1500)
+    }
+  }
 }
